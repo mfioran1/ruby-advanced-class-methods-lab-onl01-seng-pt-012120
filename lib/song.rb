269b43a8609bb13a1f57a1c13fe5cdb27a3e387d
Maurice Fiorani <mauricef97@gmail.com>
2020-02-12 07:56:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,13 @@ class Song
   end
   
   def self.find_by_name(name)
-    self.all.find{|person| person.name == name}
+    self.all.find{|x| x.name == name}
   end
+  
+  def self.find_or_create_by_name
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+    
   
 
 end
